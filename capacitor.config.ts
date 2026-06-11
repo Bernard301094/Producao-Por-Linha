e4f4e211ff9d6139f2c3b32b4cd9ff857be27030
Bernard301094 <bernard30101994@gmail.com>
2026-06-11 20:24:46 -0300

    chore: change app name to Apontamento Produção

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.vonixx.producao',
-  appName: 'Diário de Bordo',
+  appName: 'Apontamento Produção',
   webDir: 'dist',
   plugins: {
     CapacitorHttp: {
